3f15b81eabef14065389d628c74142a891c247f6
jcheron <myaddressmail@gmail.com>
2022-02-09 17:05:39 +0100

    td3

--- app/helpers/format-currency.js
+++
@@ -1,6 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export default helper(function formatCurrency(params /*, named*/) {
-  let [amount, symbol] = params;
-  return `${amount} ${symbol}`;
-});
